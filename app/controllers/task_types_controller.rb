8a9515ee86281a8575e2aa07d149c1f57b311612
Herminio Torres <herminiocesar@gmail.com>
2010-11-15 21:39:39 -0300

    adicionando tipos de tarefa a um projeto pagina 101 a 104

--- app/controllers/task_types_controller.rb
+++ app/controllers/task_types_controller.rb
@@ -25,6 +25,7 @@ class TaskTypesController < ApplicationController
   # GET /task_types/new.xml
   def new
     @task_type = TaskType.new
+    @taks_type.project = Project.find(params[:project_id])
 
     respond_to do |format|
       format.html # new.html.erb
@@ -45,7 +46,7 @@ class TaskTypesController < ApplicationController
     respond_to do |format|
       if @task_type.save
         flash[:notice] = 'TaskType was successfully created.'
-        format.html { redirect_to(@task_type) }
+        format.html { redirect_to(@task_type.project) }
         format.xml  { render :xml => @task_type, :status => :created, :location => @task_type }
       else
         format.html { render :action => "new" }
@@ -62,7 +63,7 @@ class TaskTypesController < ApplicationController
     respond_to do |format|
       if @task_type.update_attributes(params[:task_type])
         flash[:notice] = 'TaskType was successfully updated.'
-        format.html { redirect_to(@task_type) }
+        format.html { redirect_to(@task_type.project) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -78,7 +79,7 @@ class TaskTypesController < ApplicationController
     @task_type.destroy
 
     respond_to do |format|
-      format.html { redirect_to(task_types_url) }
+      format.html { redirect_to(@task_type.project) }
       format.xml  { head :ok }
     end
   end
